01891386031b4b744ec3da7a9eb268c52e63a975
Gibsol <gibsol75@gmail.com>
2023-05-19 16:25:57 +0300

    :construction: Start working on projects/ page

---
+++ src/components/Projects/ProjectContainer.tsx
@@ -0,0 +1,7 @@
+export const ProjectContainer = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return <div>{children}</div>;
+};
---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,9 @@
+import { ProjectContainer } from "./ProjectContainer";
+
+export const Projects = () => {
+  return (
+    <section id="projects" className="min-h-screen">
+      {/* <ProjectContainer></ProjectContainer> */}
+    </section>
+  );
+};
