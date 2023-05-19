58e76508f7edd9f0745f1b0529e0d752637f2116
Gibsol <gibsol75@gmail.com>
2023-05-19 16:25:42 +0300

    :sparkles: Implement Formspark in ContactForm

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,10 +1,51 @@
+import { useState } from "react";
+
+const FORMSPARK_ACTION_URL = "https://submit-form.com/MOuRUc3z";
+
 export const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [email, setEmail] = useState("");
+  const [info, setInfo] = useState("");
+
+  const clearForm = () => {
+    setName("");
+    setCompany("");
+    setEmail("");
+    setInfo("");
+  };
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+    await fetch(FORMSPARK_ACTION_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        company,
+        email,
+        info,
+      }),
+    });
+
+    clearForm();
+  };
+
   return (
-    <form id="contact" className="grid justify-center min-h-screen">
+    <form
+      onSubmit={onSubmit}
+      id="contact"
+      className="grid justify-center min-h-screen"
+    >
       <div className="flex flex-col w-full gap-5 py-48 md:max-w-lg">
         <h2 className="text-5xl">contact</h2>
         <div className="w-full form-control">
           <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
             type="text"
             name="name"
@@ -19,6 +60,8 @@ export const ContactForm = () => {
         </div>
         <div className="w-full form-control">
           <input
+            value={company}
+            onChange={(e) => setCompany(e.target.value)}
             type="text"
             name="company"
             placeholder="company"
@@ -27,6 +70,8 @@ export const ContactForm = () => {
         </div>
         <div className="w-full form-control">
           <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
             type="email"
             name="email"
@@ -41,6 +86,8 @@ export const ContactForm = () => {
         </div>
         <div className="w-full form-control">
           <textarea
+            value={info}
+            onChange={(e) => setInfo(e.target.value)}
             required
             maxLength={150}
             minLength={50}
